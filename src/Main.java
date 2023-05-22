e13881f2ea31bb892c09e49b26b71a8e12badb9b
ABalykova <asbalykova@dev.vtb.ru>
2023-05-22 22:55:11 +0400

    feat: change logic to simple math

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма чисел равна " + (firstNumber + secondNumber));
+        System.out.println("Разность чисел равна " + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел равно " + (firstNumber * secondNumber));
+        System.out.println("Частное чисел равно " + ((double) firstNumber / secondNumber));
     }
 }
