fdf33b6f0b193a9da4d5e77350c21a48a1a02f6c
ABalykova <asbalykova@dev.vtb.ru>
2023-06-06 19:44:13 +0400

    feat: change logic to scan path and check that it`s directory path

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        String path;
+        File file;
+
+        int counter = 0;
+        boolean fileExists;
+        boolean isDirectory;
+
+        while (true) {
+            path = new Scanner(System.in).nextLine();
+            file = new File(path);
+            fileExists = file.exists();
+            isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Указанный файл не существует");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Указан путь до папки. Нужно указать путь до файла");
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                counter++;
+                System.out.println("Это файл №" + counter);
+            }
+        }
     }
 }
